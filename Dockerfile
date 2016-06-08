8be98120f6bde7a3311d272f6516486b7b8aa265
Luke Roberts <email@luke-roberts.co.uk>
2016-06-08 11:08:23 +0100

    connect to redis

---
+++ Dockerfile
@@ -0,0 +1,18 @@
+FROM gliderlabs/alpine:3.2
+
+RUN apk-install docker git
+
+RUN apk-install go
+ENV GOPATH /go
+ENV PATH $GOPATH/bin:$PATH
+
+RUN go get github.com/ddollar/init
+RUN go get github.com/garyburd/redigo/redis
+
+WORKDIR /go/src/github.com/usr/app
+COPY . /go/src/github.com/usr/app
+RUN go get .
+
+ENV PORT 5000
+ENTRYPOINT ["/go/bin/init"]
+CMD ["app"]
---
+++ main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+	"html"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/garyburd/redigo/redis"
+)
+
+func main() {
+	ConnectRedis()
+
+	// read config from environment
+	port := os.Getenv("PORT")
+
+	// start server
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "<h1>Hello, %q</h1>", html.EscapeString(r.URL.Path))
+	})
+
+	log.Println("Listening=" + port)
+	log.Fatal(http.ListenAndServe(":"+port, nil))
+}
+
+func ConnectRedis() {
+	log.Print("BOO")
+	c, err := redis.DialURL(os.Getenv("REDIS_URL"))
+	if err != nil {
+		// handle connection error
+		log.Fatal(err.Error())
+	}
+	log.Print("YIPPEE")
+
+	defer c.Close()
+
+	p, err := c.Do("PING")
+	log.Printf("%#v", p)
+}
